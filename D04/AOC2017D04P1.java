f6000b33e9dda67d9b1fe2a02af6ee40c1f5ed65
Jesse Siekkinen <jesse.siekkinen@gmail.com>
2017-12-05 09:46:40 +0200

    Added Day 4 part 1.

---
+++ D04/AOC2017D04P1.java
@@ -0,0 +1,43 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+
+public class AOC2017D04P1 {
+  // Checks that passphrase doesn't contain duplicate words.
+  private static boolean isValid(String pw) {
+    String[] pwA = pw.split(" ");
+    for (int i = 0; i < pwA.length; i++) {
+      for (int i2 = 0; i2 < pwA.length; i2++) {
+        if (i == i2) {
+          continue;
+        }
+        if (pwA[i].equals(pwA[i2])) {
+          return false;
+        }
+      }
+    }
+    return true;
+  }
+
+  public static void main(String[] args) {
+    if (args.length < 1) {
+      System.out.println("Not enough arguments! Input string is needed...");
+      return;
+    }
+    try {
+      BufferedReader file = new BufferedReader(new FileReader(args[0]));
+      int numValid = 0;
+      String row;
+      while ((row = file.readLine()) != null) {
+        if (row.length() <= 0) {
+          continue;
+        }
+        numValid += isValid(row) ? 1 : 0;
+      }
+      System.out.println(numValid);
+    }
+    catch (Exception e) {
+      System.out.println("Exception occured:");
+      e.printStackTrace();
+    }
+  }
+}
