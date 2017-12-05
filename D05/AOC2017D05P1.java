e4db48948ef0ece40f1f66b9146419a917f5fef2
Jesse Siekkinen <jesse.siekkinen@gmail.com>
2017-12-05 10:35:46 +0200

    Added day 5 part 1.

---
+++ D05/AOC2017D05P1.java
@@ -0,0 +1,33 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.util.ArrayList;
+
+public class AOC2017D05P1 {
+  public static void main(String[] args) {
+    if (args.length < 1) {
+      System.out.println("Not enough arguments! Input string is needed (file name)");
+      return;
+    }
+    ArrayList<Integer> instructions = new ArrayList<>();
+    try {
+      BufferedReader file = new BufferedReader(new FileReader(args[0]));
+      String row;
+      while ((row = file.readLine()) != null) {
+        instructions.add(Integer.parseInt(row));
+      }
+    }
+    catch (Exception e) {
+      System.out.println("Exception occured:");
+      e.printStackTrace();
+    }
+    int steps = 0;
+    int i = 0;
+    while (i < instructions.size()) {
+      int prev = i;
+      i += instructions.get(prev);
+      instructions.set(prev, instructions.get(prev) + 1);
+      steps++;
+    }
+    System.out.println(steps);
+  }
+}
