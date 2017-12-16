d02a30fb7da92796fc213279c92772533c5521f6
Jesse Siekkinen <jesse.siekkinen@gmail.com>
2017-12-16 10:35:37 +0200

    Added Day 16 part 1.

---
+++ D16/AOC2017D16P1.java
@@ -0,0 +1,71 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+
+public class AOC2017D16P1 {
+  public static void main(String[] args) {
+    if (args.length < 1) {
+      System.out.println("Not enough arguments! Input string is needed (file name)");
+      return;
+    }
+    //String input = "abcde";
+    String input = "abcdefghijklmnop";
+    String put = "";
+    try {
+      BufferedReader file = new BufferedReader(new FileReader(args[0]));
+      String row;
+      while ((row = file.readLine()) != null) {
+        put += row;
+      }
+    }
+    catch (Exception e) {
+      System.out.println("Exception occured:");
+      e.printStackTrace();
+    }
+    for (String in : put.split(",")) {
+      String cmd = in.substring(0, 1);
+      String instruction = in.substring(1);
+      switch(cmd) {
+        case "s":
+          int amount = Integer.parseInt(instruction);
+          input = input.substring(input.length() - amount, input.length())
+                + input.substring(0, input.length() - amount);
+          break;
+        case "x":
+          int pos1 = Integer.parseInt(instruction.split("/")[0]);
+          int pos2 = Integer.parseInt(instruction.split("/")[1]);
+          String tmp = "";
+          for (int i = 0; i < input.length(); i++) {
+            if (i == pos1) {
+              tmp += input.charAt(pos2);
+            }
+            else if (i == pos2) {
+              tmp += input.charAt(pos1);
+            }
+            else {
+              tmp += input.charAt(i);
+            }
+          }
+          input = tmp;
+          break;
+        case "p":
+          char prog1 = instruction.split("/")[0].charAt(0);
+          char prog2 = instruction.split("/")[1].charAt(0);
+          String tmp2 = "";
+          for (int i = 0; i < input.length(); i++) {
+            if (input.charAt(i) == prog1) {
+              tmp2 += prog2;
+            }
+            else if (input.charAt(i) == prog2) {
+              tmp2 += prog1;
+            }
+            else {
+              tmp2 += input.charAt(i);
+            }
+          }
+          input = tmp2;
+          break;
+      }
+    }
+    System.out.println(input);
+  }
+}
