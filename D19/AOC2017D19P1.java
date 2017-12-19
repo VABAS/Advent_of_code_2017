7653787273cdcd0eaa438cab4b5f69b3b3341905
Jesse Siekkinen <jesse.siekkinen@gmail.com>
2017-12-19 12:13:12 +0200

    Added Day 19 part 1.

---
+++ D19/AOC2017D19P1.java
@@ -0,0 +1,101 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.util.ArrayList;
+
+public class AOC2017D19P1 {
+  private static enum Direction {UP, RIGHT, DOWN, LEFT};
+  public static void main(String[] args) {
+    if (args.length < 1) {
+      System.out.println("Not enough arguments! Input string is needed (file name)");
+      return;
+    }
+    ArrayList<ArrayList<Character>> input = new ArrayList<>();
+    int startIndex = -1;
+    try {
+      BufferedReader file = new BufferedReader(new FileReader(args[0]));
+      String row;
+      while ((row = file.readLine()) != null) {
+        input.add(new ArrayList<Character>());
+        for (int i = 0; i < row.length(); i++) {
+          if (startIndex < 0 && row.charAt(i) == '|') {
+            startIndex = input.get(0).size();
+          }
+          input.get(input.size() - 1).add(row.charAt(i));
+        }
+      }
+    }
+    catch (Exception e) {
+      System.out.println("Exception occured:");
+      e.printStackTrace();
+    }
+    /*System.out.println(startIndex);
+    for (ArrayList<Character> al : input) {
+      for (char c : al) {
+        System.out.print(c);
+      }
+      System.out.println();
+    }*/
+    int x = startIndex;
+    int y = 0;
+    Direction dir = Direction.DOWN;
+    String result = "";
+    while (true) {
+      if (dir == Direction.DOWN){
+        y++;
+      }
+      else if (dir == Direction.UP) {
+        y--;
+      }
+      if (dir == Direction.RIGHT){
+        x++;
+      }
+      else if (dir == Direction.LEFT) {
+        x--;
+      }
+      char cur;
+      try {
+        cur = input.get(y).get(x);
+      }
+      catch (IndexOutOfBoundsException e) {
+        break;
+      }
+      if (cur == ' ') {
+        break;
+      }
+      else if (cur == '+') {
+        if (dir == Direction.UP || dir == Direction.DOWN) {
+          if (x > 0) {
+            if (input.get(y).get(x - 1) != ' ' && input.get(y).get(x - 1) != '|') {
+              dir = Direction.LEFT;
+              continue;
+            }
+          }
+          if (x < input.get(y).size() - 1) {
+            if (input.get(y).get(x + 1) != ' ' && input.get(y).get(x + 1) != '|') {
+              dir = Direction.RIGHT;
+              continue;
+            }
+          }
+        }
+        if (dir == Direction.RIGHT || dir == Direction.LEFT) {
+          if (y > 0) {
+            if (input.get(y - 1).get(x) != ' ' && input.get(y - 1).get(x) != '-') {
+              dir = Direction.UP;
+              continue;
+            }
+          }
+          if (y < input.size() - 1) {
+            if (input.get(y + 1).get(x) != ' ' && input.get(y + 1).get(x) != '-') {
+              dir = Direction.DOWN;
+              continue;
+            }
+          }
+        }
+      }
+      else if (cur != '|' && cur != '-'){
+        result += cur;
+      }
+    }
+    System.out.println(result);
+  }
+}
